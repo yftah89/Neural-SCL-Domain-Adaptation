03d6fc5d744e23dca48346f01c06d800e5b420ed
Yftah Ziser <yftah89@gmail.com>
2018-06-20 04:10:38 +0300

    Keras 2 modifications

--- AE-SCL-SR/tr.py
+++ AE-SCL-SR/tr.py
@@ -8,6 +8,7 @@ from keras.callbacks import ModelCheckpoint
 import os
 import numpy as np
 from numpy.random import seed
+from keras.models import load_model
 seed(1)
 
 
@@ -27,7 +28,7 @@ def train(src,dest,pivot_num,pivot_min_st,dim):
     model = Sequential()
     #make sure the pivot matrix (the decoder) is fixed
     frozen_layer = Dense(outputs, trainable=False)
-    model.add(Dense(HUs,input_shape=(inputs,),init='glorot_normal'))
+    model.add(Dense(HUs, kernel_initializer='glorot_normal', input_shape=(inputs,)))
     model.add(Activation('sigmoid'))
     model.add(frozen_layer)
     model.add(Activation('sigmoid'))
@@ -45,12 +46,13 @@ def train(src,dest,pivot_num,pivot_min_st,dim):
     #saveing only the best model 
     save_best = ModelCheckpoint("best_model", monitor='val_loss', verbose=0, save_best_only=True, mode='auto')
 
-    h=model.fit(x, y, batch_size=1,callbacks=[earlyStopping],nb_epoch=10,validation_data=(x_valid,y_valid), shuffle=True)
+    h=model.fit(x, y, batch_size=1,callbacks=[earlyStopping,save_best],epochs=10,validation_data=(x_valid,y_valid), shuffle=True)
     print (h.history['val_loss'])[-1]
     weight_str = src + "_to_" + dest + "/weights/w_"+src+"_"+dest+"_"+str(pivot_num)+"_"+str(pivot_min_st)+"_"+str(HUs)
     filename = weight_str
     if not os.path.exists(os.path.dirname(filename)):
             os.makedirs(os.path.dirname(filename))
     #saving the entire model
+    model = load_model("best_model")
     np.save(weight_str, model.get_weights())
 
